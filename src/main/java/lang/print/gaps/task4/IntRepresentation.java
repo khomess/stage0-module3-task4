b04f5487a13d13ee9124487576d20c928a383eef
khomess <sasha.khomenko12@gmail.com>
2022-11-15 13:20:57 +0200

    Primitives

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
+        char k =(char)c;
+        char l =(char)h;
+        char n =(char)a;
+        char m =(char)r;
+
+        System.out.println(k + l + n + m);
     }
 }
